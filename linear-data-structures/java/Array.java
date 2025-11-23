6251b3cef1bfc4e3137829284b65af389df54ea9
Ebube Ugwu <ugwuebube3@gmail.com>
2025-11-23 18:15:07 +0100

    Array data structure in java

---
+++ linear-data-structures/java/Array.java
@@ -0,0 +1,59 @@
+public class Array {
+    private int count = 0;
+    private int[] items;
+    
+    public Array(int length) {
+        items = new int[length];
+    }
+
+    public void insert(int item) {
+        if (count >= items.length) {
+            grow();
+        }
+        items[count++] = item;
+    }
+    public int length() {
+        return count;
+    }
+
+    public int itemAt(int index) {
+        if (index >= items.length) {
+            throw new IllegalArgumentException("Index out of bounds");
+        }
+        return items[index];
+    }
+
+    public void removeAt(int index) {
+        if (index >= items.length) {
+            throw new IllegalArgumentException("Index out of bounds");
+        }
+        shift(index);
+        count--;
+    }
+
+    public int indexOf(int target) {
+        int index = 0;
+        for (var item : items) {
+            if (item == target) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+
+    private void shift(int index) {
+        for (int i = index; i < count; i++) {
+            items[i] = items[i+1];
+        }
+    }
+
+
+    private void grow() {
+        int[] newArray = new int[items.length * 2];
+        for (int i = 0; i < items.length; i++) {
+            newArray[i] = items[i];
+        }
+        items = newArray;
+    }
+}
